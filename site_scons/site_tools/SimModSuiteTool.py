901d24ae7ae00e7f551a5fe8635caa4200fe7720
Sebastian Rettenberger <rettenbs@in.tum.de>
2016-04-01 13:28:53 +0200

    SimModTool improvements
    Detect simmodeler directory

--- site_scons/site_tools/SimModSuiteTool.py
+++ site_scons/site_tools/SimModSuiteTool.py
@@ -7,11 +7,47 @@
 #  notice in the file 'COPYING' at the root directory of this package
 #  and the copyright notice at https://github.com/TUM-I5/PUML
 #
-# @copyright 2014 Technische Universitaet Muenchen
+# @copyright 2014-2016 Technische Universitaet Muenchen
 # @author Sebastian Rettenberger <rettenbs@in.tum.de>
 #
 
 import os
+
+def tryLibPath(env, conf, libPath, mpiWrapper, setRpath):
+    oldLibPath = env['LIBPATH']
+    oldRPath = env['RPATH']
+
+    success = False
+
+    try:
+        env.AppendUnique(LIBPATH=libPath)
+
+        # TODO check which headers belong to which library
+        if not conf.CheckLibWithHeader('SimMeshing', 'MeshSim.h', 'c++'):
+            return False
+
+        # Add path for parasolid library
+        psLibPath = [p for p in map(lambda p: os.path.join(p, 'psKrnl'), env['LIBPATH']) if os.path.exists(p)]
+        env.AppendUnique(LIBPATH=psLibPath)
+        if setRpath:
+            env.AppendUnique(RPATH=psLibPath)
+
+        # TODO not all libraries may be available/required
+        # TODO different parasolid versions not handled currently
+        libs = ['SimField', 'SimDiscrete', 'SimAdvMeshing', 'SimMeshTools', 'SimParasolid270',
+                'SimPartitionedMesh-mpi', 'SimPartitionWrapper-'+mpiWrapper, 'SimModel', 'pskernel']
+        for l in libs:
+            if not conf.CheckLib(l):
+                return False
+
+        success = True
+    finally:
+        if not success:
+            env['LIBPATH'] = oldLibPath
+            env['RPATH'] = oldRPath
+
+    return True
+
 
 def generate(env, **kw):
     conf = env.Configure()
@@ -32,33 +68,21 @@ def generate(env, **kw):
         mpi = 'mpich2'
 
     # TODO support different configurations
-    libPath = [p for p in map(lambda p: os.path.join(p, 'x64_rhel5_gcc41'), env['LIBPATH']) if os.path.exists(p)]
-    env.AppendUnique(LIBPATH=libPath)
+    for lib in ['x64_rhel5_gcc41', 'x64_rhel6_gcc44']:
+        libPath = [p for p in map(lambda p: os.path.join(p, lib), env['LIBPATH']) if os.path.exists(p)]
+        if not libPath:
+            # This library directory does not exist -> try next
+            continue
 
-    # TODO check which headers belong to which library
-    if not conf.CheckLibWithHeader('SimMeshing', 'MeshSim.h', 'c++'):
-        if required:
-            print 'Could not find SimModSuite!'
-            env.Exit(1)
-        else:
+        print 'Checking for simmodeler libraries in ' + lib + '...'
+        if tryLibPath(env, conf, libPath, mpi, rpath):
             conf.Finish()
             return
 
-    # Add path for parasolid library
-    libPath = [p for p in map(lambda p: os.path.join(p, 'psKrnl'), env['LIBPATH']) if os.path.exists(p)]
-    env.AppendUnique(LIBPATH=libPath)
-    if rpath:
-        env.AppendUnique(RPATH=libPath)
-
-    # TODO not all libraries may be available/required
-    # TODO different parasolid versions not handled currently
-    libs = ['SimField', 'SimDiscrete', 'SimAdvMeshing', 'SimMeshTools', 'SimParasolid270',
-            'SimPartitionedMesh-mpi', 'SimPartitionWrapper-'+mpi, 'SimModel', 'pskernel']
-    for l in libs:
-        if not conf.CheckLib(l):
-            if required:
-                print 'Could not find SimModSuite. ' + l + ' is missing!'
-                env.Exit(1)
+    # Not found
+    if required:
+        print 'Could not find SimModSuite!'
+        env.Exit(1)
 
     conf.Finish()
 
